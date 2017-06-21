d2198725cb25e9226cf9ef1f26ecccfca71d877e
charlieliv129 <cdl32@duke.edu>
2017-06-21 10:00:05 -0500

    Update motion.c

--- motion.c
+++ motion.c
@@ -1,129 +1,133 @@
-// This #include statement was automatically added by the Particle IDE.
-#include <Adafruit_ST7735.h>
+PRODUCT_ID(4351);
+PRODUCT_VERSION(1);
 
 // This #include statement was automatically added by the Particle IDE.
-#include <PietteTech_DHT.h>
+#include <Adafruit_ST7735.h>
+// This #include statement was automatically added by the Particle IDE.
+
 
 //SCL to A3
 //SDA to A5
 //GND to GND
 //VCC to VIN
 
-#define DHTTYPE DHT22
-#define DHTPIN D3
 #define cs A2 //chip select for LCD
 #define dc D0
 #define rst 0
 
 
-int sampleInt = 60000;
-int tempf;
-double tempfD;
-double tempfD2;
-double temperatureD;
-int temp;
-int humidity;
-double humidityD;
-int dew;
-int blue = D7;
-String location = "BTC_CBD_1";
+#define inputPin D2  // choose the input pin (for PIR sensor)
+#define ledPin D7
 
-Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst); //hardware spi
-PietteTech_DHT DHT(DHTPIN, DHTTYPE);
+int val = LOW;   // variable for reading the pin status
+int pirState = LOW;             // we start, assuming no motion detected
+String state = "";
 
+Adafruit_ST7735 tft = Adafruit_ST7735(cs,dc,rst);
 
 void setup() {
-    pinMode(blue, OUTPUT);
-    //Variable exposed to cloud
-    Particle.variable("Temperature", temp);
-    Particle.variable("Humidity", humidity);
-    Particle.variable("DewPt", dew);
-    Particle.variable("Location", location);
-  
+    Serial.begin(9600);
+    pinMode(inputPin, INPUT); //sensor
+    pinMode(ledPin, OUTPUT); //led
     tft.initG();
     tft.fillScreen(ST7735_BLACK); //setting up screen
     tft.setTextColor(ST7735_GREEN);
     tft.setTextSize(2);
     tft.setCursor(0,0);
-    
-    Serial.begin(9600);
-    
-    //Particle.variable("Temp", &tempF, DOUBLE);
-    
 }
 
 void loop() {
-    
-    int result = DHT.acquireAndWait(1000); //wait up to 1 sec (default indefinitely)
-    
-    switch (result){
-        case DHTLIB_OK:
-            Serial.println("OK");
-            break;
-        case DHTLIB_ERROR_CHECKSUM:
-            Serial.println("Error\n\r\tChecksum error");
-            break;
-        case DHTLIB_ERROR_ISR_TIMEOUT:
-            Serial.println("Error\n\r\tISR time out error");
-            break;
-        case DHTLIB_ERROR_RESPONSE_TIMEOUT:
-            Serial.println("Error\n\r\tResponse time out error");
-            break;
-        case DHTLIB_ERROR_DATA_TIMEOUT:
-            Serial.println("Error\n\r\tData time out error");
-            break;
-        case DHTLIB_ERROR_ACQUIRING:
-            Serial.println("Error\n\r\tAcquiring");
-            break;
-        case DHTLIB_ERROR_DELTA:
-            Serial.println("Error\n\r\tDelta time too small");
-            break;
-        case DHTLIB_ERROR_NOTSTARTED:
-            Serial.println("Error\n\r\tNot started");
-            break;        
-        default:
-            Serial.println("Unkown error");
-            break;
+    digitalWrite(ledPin, LOW); //led off at start
+    bool status = false;
+    int i = 0; //start loop over
+    int tag = -1;
+    state = "0";
+    val = LOW; //reset everything to no motion seen
+    while (i < 60)
+    {
+        val = digitalRead(inputPin); //read input value
+        if (status == true) //already registered motion in last 30 seconds
+        {
+            if (val == LOW) //if there is no motion at this time
+            {
+                digitalWrite(ledPin, LOW); //turn the led off
+                if (tag != 2)
+                {
+                    tag = 2;
+                    motionOnCyclePast(); //display that there is no current motion, but there was on the current cycle
+                }
+            }
+            else 
+            {
+                digitalWrite(ledPin, HIGH); //if motion turn on the led
+                if (tag != 1)
+                {
+                    tag = 1;
+                    motionOnCycleCurrent(); //say that there was and is motion
+                }
+            }   
+            i++;
+            state = "1"; //motion occurred
+            delay(1000);
+            continue;
+        }
+        if (val == HIGH) //if motion for first time
+        {
+            digitalWrite(ledPin, HIGH); //led on 
+            status = true; //has seen motion
+            if (tag != 1)
+            {
+                motionOnCycleCurrent(); //motion occurred in cycle and now
+                tag = 1; //to prevent flashing same text repeatedly 
+            }
+        }
+        if (tag == -1)
+        {
+            noMotionOnCycle(); //no motion yet 
+            tag = 0;
+        }
+        delay(1000);
+        i++;
     }
+    tft.setTextColor(ST7735_GREEN);
+    tft.println("End Cycle"); //when cycle is over 
     
-    Serial.print("Temp:   ");
-    Serial.println(DHT.getFahrenheit(), 2);
-    Serial.print("Humi:   ");
-    Serial.println(DHT.getHumidity(), 2);
-    Serial.print("Dew Pt:   ");
-    Serial.println(DHT.getDewPoint(), 2);
-    Serial.println(Time.timeStr());
-    Serial.println();
-    
-    temp = DHT.getFahrenheit();
-    humidity = DHT.getHumidity();
-    dew = DHT.getDewPoint();
-    Serial.println(humidity);
-    humidityD = DHT.getHumidity();
-    tempf = DHT.getFahrenheit() * 100;
-    tempfD = DHT.getFahrenheit();
-    temperatureD = tempfD;
-    
-    digitalWrite(blue, HIGH);
-    Particle.publish("Location", location, PRIVATE); //unit location here
-    Particle.publish("Temperature", String(temp), PRIVATE);
-    Particle.publish("Humidity", String(humidity), PRIVATE);
-    Particle.publish("DewPt", String(dew*1.8+32), PRIVATE);
-    delay(500);
-    digitalWrite(blue, LOW);
-    displayData();
-    delay(sampleInt);
+    //Particle.publish("Movement", state, PRIVATE); //publish results of the cycle
+    Particle.publish("motion", state, PRIVATE); //azure
 }
 
-void displayData()
+void noMotionOnCycle() //tag = 0
 {
-    tft.fillScreen(ST7735_BLACK); //setting up screen
-    tft.setTextSize(2);
+    tft.fillScreen(ST7735_BLACK);
     tft.setCursor(0,0);
-    tft.print("Temp: ");
-    tft.println(DHT.getFahrenheit(),1);
-    tft.print("Hum: ");
-    tft.println(DHT.getHumidity(),1);
-    tft.print("DewPt: ");
-    tft.println(DHT.getDewPoint()*1.8+32, 1);
+    tft.setTextColor(ST7735_GREEN);
+    tft.println("Cycle\nStatus:");
+    tft.setTextColor(ST7735_RED);
+    tft.println("No Motion");
+}
+void motionOnCycleCurrent() //tag = 1
+{
+    tft.setTextColor(ST7735_GREEN);
+    tft.fillScreen(ST7735_BLACK);
+    tft.setCursor(0,0);
+    tft.println("Cycle\nStatus:");
+    tft.setTextColor(ST7735_RED);
+    tft.println("Motion\nRegistered");
+    tft.setTextColor(ST7735_GREEN);
+    tft.println("Current\nStatus:");
+    tft.setTextColor(ST7735_RED);
+    tft.println("Motion\nDetected");
+}
+void motionOnCyclePast() //tag = 2
+{
+    tft.setTextColor(ST7735_GREEN);
+    tft.fillScreen(ST7735_BLACK);
+    tft.setCursor(0,0);
+    tft.println("Cycle\nStatus:");
+    tft.setTextColor(ST7735_RED);
+    tft.println("Motion\nRegistered");
+    tft.setTextColor(ST7735_GREEN);
+    tft.println("Current\nStatus:");
+    tft.setTextColor(ST7735_RED);
+    tft.println("No Motion\nDetected");
 }
